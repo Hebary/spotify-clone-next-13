47c582007b7e334a0c853b53647dc27e4ccb741c
shakharho <berdaddario@gmail.com>
2023-09-10 08:58:50 -0300

    feat: ListItem component group className usage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,8 +9,12 @@ const HomePage = () => {
           <div className='mb-2'>
             <h1 className='text-white text-3xl font-semibold'>Welcome back</h1>
           </div>
-          <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4 '>
-            <ListItem/>
+          <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4 p-2'>
+            <ListItem
+              image='/images/liked.png'
+              name='Liked Songs'
+              href='/images/liked.png'
+            />
           </div>
       </Header>
     </div>
--- app/components/ui/ListItem.tsx
+++ app/components/ui/ListItem.tsx
@@ -1,13 +1,48 @@
 'use client';
 
 import { FC } from 'react'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { FaPlay } from 'react-icons/fa';
 
 
-interface Props{}
+interface Props{
+    image: string;
+    name : string;
+    href : string;
+}
 
-const ListItem:FC<Props> = ({}) => {
-     return (
-        <div> ListItem </div>
+const ListItem:FC<Props> = ({ name, image, href }) => {
+    
+    const router = useRouter();
+    const onClick = () => {
+        //Auht before push
+        router.push(href)
+    }
+
+
+    return (
+        // group className Usage
+        <button
+            className='relative group flex items-center rounded-md gap-x-4 overflow-hidden bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4 cursor-pointer'
+        >
+            
+            <div className='relative min-h-[64px] min-w-[64px]'>
+                <Image
+                    className='object-cover'
+                    fill
+                    src={"/images/liked.png"}
+                    alt={name}
+                />
+            </div>     
+            <p className='font-medium truncate py-5'>
+                {name}
+            </p>
+            
+            <div className='absolute transition opacity-0 rounded-full flex items-center p-4 justify-center bg-green-500 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110'>
+                <FaPlay className='text-black'/>
+            </div>
+        </button>
     )
 }
 
